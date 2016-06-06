25afe420eed2bd7b9d0c502ce766691d81b3a0be
Andrei Tomashpolskiy <nordmann89@gmail.com>
2016-06-06 23:29:40 +0300

    The last block of the last chunk can be truncated -- should handle properly

--- src/main/java/bt/data/DataDescriptor.java
+++ src/main/java/bt/data/DataDescriptor.java
@@ -82,6 +82,12 @@ public class DataDescriptor implements IDataDescriptor {
                     // new chunk will begin with the next file
                     firstFileInChunkIndex++;
                     chunkOffset = 0;
+                } else if (currentFileIndex == filesCount - 1) {
+                    // create chunk for the remainder of the last file
+                    chunkDescriptors.add(new ChunkDescriptor(
+                            Arrays.copyOfRange(files, firstFileInChunkIndex, currentFileIndex + 1),
+                            chunkOffset, fileSize, chunkHashes.next(), configurationService.getTransferBlockSize()
+                    ));
                 }
             }
         }
