0ae9d4b847073110897c1a1b8a45abf79d037101
Andrei Tomashpolskiy <nordmann89@gmail.com>
2016-12-02 21:11:44 +0300

    Publish latest javadocs with Travis

---
+++ .travis/publish-javadoc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "atomashpolskiy/bt" ] && [$TRAVIS_OS_NAME == "osx"] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+
+  rm -rf $HOME/gh-pages
+  mkdir -p $HOME/gh-pages/javadoc/latest
+  cd $HOME/gh-pages
+
+  git init
+  git remote add javadoc https://${TRAVIS_TOKEN}@github.com/atomashpolskiy/bt.git
+  git fetch --depth=1 javadoc gh-pages
+
+  cp -R $TRAVIS_BUILD_DIR/target/site/apidocs $HOME/gh-pages/javadoc/latest
+
+  git add --all
+  git commit -m "latest javadoc for commit $TRAVIS_COMMIT (build $TRAVIS_BUILD_NUMBER)"
+  git merge --no-edit -s ours remotes/javadoc/gh-pages
+  git push javadoc master:gh-pages
+
+fi
