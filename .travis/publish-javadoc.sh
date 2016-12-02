9dd3b9221d43020a96b914cd403a615c731dc9bf
Andrei Tomashpolskiy <nordmann89@gmail.com>
2016-12-02 22:54:10 +0300

    update script

--- .travis/publish-javadoc.sh
+++ .travis/publish-javadoc.sh
@@ -5,19 +5,21 @@ if [ "$TRAVIS_REPO_SLUG" == "atomashpolskiy/bt" ] && [ "$TRAVIS_OS_NAME" == "lin
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
 
-  rm -rf $HOME/gh-pages
-  mkdir -p $HOME/gh-pages/javadoc/latest
-  cd $HOME/gh-pages
+  cd $HOME
+  rm -rf gh-pages
 
-  git init
-  git remote add javadoc https://${TRAVIS_TOKEN}@github.com/atomashpolskiy/bt.git
-  git fetch --depth=1 javadoc gh-pages
+  git clone https://${TRAVIS_TOKEN}@github.com/atomashpolskiy/bt.git gh-pages
+  cd gh-pages
+  git checkout gh-pages
 
-  cp -R $TRAVIS_BUILD_DIR/target/site/apidocs $HOME/gh-pages/javadoc/latest
+  rm -rf javadoc/latest
+  mkdir -p javadoc/latest
+
+  cp -R $TRAVIS_BUILD_DIR/target/site/apidocs/* $HOME/gh-pages/javadoc/latest
 
   git add --all
   git commit -m "latest javadoc for commit $TRAVIS_COMMIT (build $TRAVIS_BUILD_NUMBER)"
-  git merge --no-edit -s ours remotes/javadoc/gh-pages
-  git push javadoc master:gh-pages
+  # prevent showing the token in travis logs
+  git push origin HEAD:gh-pages > /dev/null
 
 fi
