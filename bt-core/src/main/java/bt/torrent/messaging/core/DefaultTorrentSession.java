5a6fa6779e51e7fb72bc1d2287fb1daec55849fe
Andrei Tomashpolskiy <nordmann89@gmail.com>
2016-12-05 00:43:02 +0300

    Fix amounts/rates calculation bug

--- bt-core/src/main/java/bt/torrent/messaging/core/DefaultTorrentSession.java
+++ bt-core/src/main/java/bt/torrent/messaging/core/DefaultTorrentSession.java
@@ -8,11 +8,17 @@ import bt.net.Peer;
 import bt.torrent.Bitfield;
 import bt.torrent.TorrentSession;
 import bt.torrent.TorrentSessionState;
+import bt.torrent.messaging.ConnectionState;
 import bt.torrent.messaging.IPeerWorkerFactory;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 /**
  *<p><b>Note that this class implements a service.
@@ -94,10 +100,31 @@ class DefaultTorrentSession implements TorrentSession {
 
     private class DefaultTorrentSessionState implements TorrentSessionState {
 
+        private static final int DOWNLOADED_POSITION = 0;
+        private static final int UPLOADED_POSITION = 1;
+
+        /**
+         * Recently calculated amounts of downloaded and uploaded data
+         */
+        private Map<Peer, Long[]> recentAmountsForConnectedPeers;
+
+        /**
+         * Historical data (amount of data downloaded from disconnected peers)
+         */
+        private volatile AtomicLong downloadedFromDisconnected;
+
+        /**
+         * Historical data (amount of data uploaded to disconnected peers)
+         */
+        private volatile AtomicLong uploadedToDisconnected;
+
         private Bitfield localBitfield;
 
         DefaultTorrentSessionState(Bitfield localBitfield) {
             this.localBitfield = localBitfield;
+            this.recentAmountsForConnectedPeers = new HashMap<>();
+            this.downloadedFromDisconnected = new AtomicLong();
+            this.uploadedToDisconnected = new AtomicLong();
         }
 
         @Override
@@ -111,23 +138,47 @@ class DefaultTorrentSession implements TorrentSession {
         }
 
         @Override
-        public long getDownloaded() {
-
-            long downloaded = 0;
-            for (Peer peer : worker.getPeers()) {
-                downloaded += worker.getConnectionState(peer).getDownloaded();
-            }
+        public synchronized long getDownloaded() {
+            long downloaded = getCurrentAmounts().values().stream()
+                    .collect(Collectors.summingLong(amounts -> amounts[DOWNLOADED_POSITION]));
+            downloaded += downloadedFromDisconnected.get();
             return downloaded;
         }
 
         @Override
-        public long getUploaded() {
-
-            long uploaded = 0;
-            for (Peer peer : worker.getPeers()) {
-                uploaded += worker.getConnectionState(peer).getUploaded();
-            }
+        public synchronized long getUploaded() {
+            long uploaded = getCurrentAmounts().values().stream()
+                    .collect(Collectors.summingLong(amounts -> amounts[UPLOADED_POSITION]));
+            uploaded += uploadedToDisconnected.get();
             return uploaded;
+        }
+
+        private synchronized Map<Peer, Long[]> getCurrentAmounts() {
+            Map<Peer, Long[]> connectedPeers = getAmountsForConnectedPeers();
+            connectedPeers.forEach((peer, amounts) -> recentAmountsForConnectedPeers.put(peer, amounts));
+
+            Set<Peer> disconnectedPeers = new HashSet<>();
+            recentAmountsForConnectedPeers.forEach((peer, amounts) -> {
+                if (!connectedPeers.containsKey(peer)) {
+                    downloadedFromDisconnected.addAndGet(amounts[DOWNLOADED_POSITION]);
+                    uploadedToDisconnected.addAndGet(amounts[UPLOADED_POSITION]);
+                    disconnectedPeers.add(peer);
+                }
+            });
+            disconnectedPeers.forEach(recentAmountsForConnectedPeers::remove);
+
+            return recentAmountsForConnectedPeers;
+        }
+
+        private Map<Peer, Long[]> getAmountsForConnectedPeers() {
+            return worker.getPeers().stream()
+                    .collect(
+                            HashMap::new,
+                            (acc, peer) -> {
+                                ConnectionState connectionState = worker.getConnectionState(peer);
+                                acc.put(peer, new Long[] {connectionState.getDownloaded(), connectionState.getUploaded()});
+                            },
+                            HashMap::putAll);
         }
 
         @Override
