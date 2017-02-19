77565f09afc74f43b87a08f42ed67f3ec8f961e6
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-02-19 22:56:05 +0300

    Use ScheduledPeerSource in DHT module

--- bt-dht/src/main/java/bt/dht/DHTPeerSource.java
+++ bt-dht/src/main/java/bt/dht/DHTPeerSource.java
@@ -2,104 +2,41 @@ package bt.dht;
 
 import bt.metainfo.Torrent;
 import bt.net.Peer;
-import bt.peer.PeerSource;
+import bt.peer.ScheduledPeerSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
  * @since 1.1
  */
-public class DHTPeerSource implements PeerSource {
+public class DHTPeerSource extends ScheduledPeerSource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DHTPeerSource.class);
 
-    private static final int MAX_PEERS_PER_BATCH = 10;
+    private static final int MAX_PEERS_PER_COLLECTION = 50;
 
     private final Torrent torrent;
     private final DHTService dhtService;
-    private final ExecutorService executor;
-
-    private final ReentrantLock lock;
-    private final AtomicReference<Future<?>> futureOptional;
-    private final Queue<Peer> peers;
 
     DHTPeerSource(Torrent torrent, DHTService dhtService, ExecutorService executor) {
+        super(executor);
         this.torrent = torrent;
         this.dhtService = dhtService;
-        this.executor = executor;
-
-        this.lock = new ReentrantLock();
-        this.futureOptional = new AtomicReference<>();
-        this.peers = new LinkedBlockingQueue<>();
     }
 
     @Override
-    public boolean update() {
-        if (peers.isEmpty()) {
-            schedulePeerCollection();
-        }
-        return !peers.isEmpty();
-    }
-
-    private void schedulePeerCollection() {
-        if (lock.tryLock()) {
-            try {
-                if (futureOptional.get() != null) {
-                    Future<?> future = futureOptional.get();
-                    if (future.isDone()) {
-                        try {
-                            future.get();
-                        } catch (InterruptedException | ExecutionException e) {
-                            LOGGER.warn("Peer collection finished with exception for torrent ID: " + torrent.getTorrentId(), e);
-                        }
-                        futureOptional.set(null);
-                    }
-                }
-
-                if (futureOptional.get() == null) {
-                    futureOptional.set(executor.submit(this::collectPeers));
-                    LOGGER.info("Scheduled peer collection for torrent ID: " + torrent.getTorrentId());
-                }
-            } finally {
-                lock.unlock();
-            }
-        }
-    }
-
-    private void collectPeers() {
-        Stream<Peer> peerStream = dhtService.getPeers(torrent);
+    protected void collectPeers(Consumer<Peer> peerConsumer) {
+        Stream<Peer> peerStream = dhtService.getPeers(torrent).limit(MAX_PEERS_PER_COLLECTION);
         peerStream.forEach(peer -> {
-            peers.add(peer);
+            peerConsumer.accept(peer);
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug(String.format("Collected new peer (torrent ID: %s, peer: %s)", torrent.getTorrentId(), peer));
             }
         });
         LOGGER.info("Peer collection finished for torrent ID: " + torrent.getTorrentId());
-    }
-
-    @Override
-    public Collection<Peer> getPeers() {
-        Set<Peer> batch = new HashSet<>((int)(MAX_PEERS_PER_BATCH / 0.75d + 1));
-        int i = 0;
-        Peer peer;
-        while (i++ < MAX_PEERS_PER_BATCH && (peer = peers.poll()) != null) {
-            batch.add(peer);
-        }
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Collected a batch of " + batch.size() + " peers: " + batch);
-        }
-        return batch;
     }
 }
