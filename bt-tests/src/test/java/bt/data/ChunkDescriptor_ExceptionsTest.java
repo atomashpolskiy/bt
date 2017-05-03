7aa0609faf09bf1e239896dee6c28a40fc08d12c
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-05-03 22:22:59 +0300

    Remove outdated tests

--- bt-tests/src/test/java/bt/data/ChunkDescriptor_ExceptionsTest.java
+++ bt-tests/src/test/java/bt/data/ChunkDescriptor_ExceptionsTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static bt.TestUtil.assertExceptionWithMessage;
 import static bt.data.ChunkDescriptorTestUtil.mockStorageUnit;
-import static org.junit.Assert.assertEquals;
 
 public class ChunkDescriptor_ExceptionsTest {
 
@@ -18,7 +17,9 @@ public class ChunkDescriptor_ExceptionsTest {
     @Test
     public void testExceptions_TooManyBlocks() {
         assertExceptionWithMessage(
-                it -> new DefaultChunkDescriptor(new StorageUnit[1], 0, Long.MAX_VALUE, 1, new byte[20], null, false),
+                it -> new DefaultChunkDescriptor(
+                        new StorageUnit[]{ChunkDescriptorTestUtil.mockStorageUnit(Long.MAX_VALUE)},
+                        0, Long.MAX_VALUE, 1, new byte[20], null, false),
                 "Integer overflow while constructing chunk: too many blocks");
     }
 
@@ -26,50 +27,5 @@ public class ChunkDescriptor_ExceptionsTest {
     public void testExceptions_WriteEmptyBlock_NoException() {
         ChunkDescriptor chunk = new DefaultChunkDescriptor(new StorageUnit[]{mockStorageUnit(10)}, 0, 10, 5, new byte[20], null, false);
         chunk.getData().putBytes(new byte[0]);
-    }
-
-    @Test
-    // TODO: move this to ReadWriteDataRangeTest
-    public void testExceptions_Write_NegativeOffset() {
-        ChunkDescriptor chunk = new DefaultChunkDescriptor(new StorageUnit[]{mockStorageUnit(1)}, 0, 1, 1, new byte[20], null, false);
-        assertExceptionWithMessage(
-                it -> {chunk.getData().getSubrange(-1).putBytes(new byte[1]); return null;},
-                "Illegal arguments: offset (-1)");
-    }
-
-    @Test
-    // TODO: move this to ReadWriteDataRangeTest
-    public void testExceptions_Read_NegativeOffset() {
-        ChunkDescriptor chunk = new DefaultChunkDescriptor(new StorageUnit[]{mockStorageUnit(1)}, 0, 1, 1, new byte[20], null, false);
-        assertExceptionWithMessage(
-                it -> chunk.getData().getSubrange(-1, 1),
-                "Illegal arguments: offset (-1), length (1)");
-    }
-
-    @Test
-    // TODO: move this to ReadWriteDataRangeTest
-    public void testExceptions_Read_NegativeLength() {
-        ChunkDescriptor chunk = new DefaultChunkDescriptor(new StorageUnit[]{mockStorageUnit(1)}, 0, 1, 1, new byte[20], null, false);
-        assertExceptionWithMessage(
-                it -> chunk.getData().getSubrange(0, -1),
-                "Illegal arguments: offset (0), length (-1)");
-    }
-
-    @Test
-    // TODO: move this to ReadWriteDataRangeTest
-    public void testExceptions_Read_ZeroLength_Exception() {
-        ChunkDescriptor chunk = new DefaultChunkDescriptor(new StorageUnit[]{mockStorageUnit(1)}, 0, 1, 1, new byte[20], null, false);
-        assertExceptionWithMessage(
-                it -> chunk.getData().getSubrange(0, 0),
-                "Illegal arguments: offset (0), length (0)");
-    }
-
-    @Test
-    // TODO: move this to ReadWriteDataRangeTest
-    public void testExceptions_Read_RequestedTooMuchData() {
-        ChunkDescriptor chunk = new DefaultChunkDescriptor(new StorageUnit[]{mockStorageUnit(1)}, 0, 1, 1, new byte[20], null, false);
-        assertExceptionWithMessage(
-                it -> chunk.getData().getSubrange(0, Integer.MAX_VALUE),
-                "Insufficient data (offset: 0, requested block length: 2147483647)");
     }
 }
