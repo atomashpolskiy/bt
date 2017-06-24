8e9eacc047ec6ab7118a47c34fb8c73ef3557b24
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-06-24 16:28:24 +0300

    BEP-9: Magnet links

---
+++ bt-core/src/main/java/bt/magnet/UtMetadata.java
@@ -0,0 +1,102 @@
+package bt.magnet;
+
+import bt.protocol.extended.ExtendedMessage;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class UtMetadata extends ExtendedMessage {
+    enum Type {
+        REQUEST(0), DATA(1), REJECT(2);
+
+        private final int id;
+
+        Type(int id) {
+            this.id = id;
+        }
+
+        int id() {
+            return id;
+        }
+
+        static Type forId(int id) {
+            for (Type type : values()) {
+                if (type.id == id) {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("Unknown message id: " + id);
+        }
+    }
+
+    private static final String id = "ut_metadata";
+    private static final String messageTypeField = "msg_type";
+    private static final String pieceIndexField = "piece";
+    private static final String totalSizeField = "total_size";
+
+    static String id() {
+        return id;
+    }
+
+    static String messageTypeField() {
+        return messageTypeField;
+    }
+
+    static String pieceIndexField() {
+        return pieceIndexField;
+    }
+
+    static String totalSizeField() {
+        return totalSizeField;
+    }
+
+    public static UtMetadata request(int pieceIndex) {
+        return new UtMetadata(Type.REQUEST, pieceIndex);
+    }
+
+    public static UtMetadata data(int pieceIndex, int totalSize, byte[] data) {
+        return new UtMetadata(Type.DATA, pieceIndex, totalSize, Objects.requireNonNull(data));
+    }
+
+    public static UtMetadata reject(int pieceIndex) {
+        return new UtMetadata(Type.REJECT, pieceIndex);
+    }
+
+    private final Type type;
+    private final int pieceIndex;
+    private final Optional<Integer> totalSize;
+    private final Optional<byte[]> data;
+
+    UtMetadata(Type type, int pieceIndex) {
+        this(type, pieceIndex, null, null);
+    }
+
+    UtMetadata(Type type, int pieceIndex, Integer totalSize, byte[] data) {
+        if (pieceIndex < 0) {
+            throw new IllegalArgumentException("Invalid piece index: " + pieceIndex);
+        }
+        if (totalSize != null && totalSize <= 0) {
+            throw new IllegalArgumentException("Invalid total size: " + totalSize);
+        }
+        this.type = type;
+        this.pieceIndex = pieceIndex;
+        this.totalSize = Optional.ofNullable(totalSize);
+        this.data = Optional.ofNullable(data);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getPieceIndex() {
+        return pieceIndex;
+    }
+
+    public Optional<byte[]> getData() {
+        return data;
+    }
+
+    public Optional<Integer> getTotalSize() {
+        return totalSize;
+    }
+}
---
+++ bt-core/src/main/java/bt/magnet/UtMetadataMessageHandler.java
@@ -0,0 +1,114 @@
+package bt.magnet;
+
+import bt.bencoding.BEParser;
+import bt.bencoding.model.BEInteger;
+import bt.bencoding.model.BEMap;
+import bt.bencoding.model.BEObject;
+import bt.protocol.DecodingContext;
+import bt.protocol.handler.MessageHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class UtMetadataMessageHandler implements MessageHandler<UtMetadata> {
+    private final Collection<Class<? extends UtMetadata>> supportedTypes = Collections.singleton(UtMetadata.class);
+
+    @Override
+    public boolean encode(UtMetadata message, ByteBuffer buffer) {
+        boolean encoded = false;
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            writeMessage(message, bos);
+            byte[] payload = bos.toByteArray();
+            if (buffer.remaining() >= payload.length) {
+                buffer.put(payload);
+                encoded = true;
+            }
+        } catch (IOException e) {
+            // can't happen
+        }
+        return encoded;
+    }
+
+    private void writeMessage(UtMetadata message, OutputStream out) throws IOException {
+        BEMap m = new BEMap(null, new HashMap<String, BEObject<?>>() {{
+            put(UtMetadata.messageTypeField(), new BEInteger(null, BigInteger.valueOf(message.getType().id())));
+            put(UtMetadata.pieceIndexField(), new BEInteger(null, BigInteger.valueOf(message.getPieceIndex())));
+            if (message.getData().isPresent()) {
+                put(UtMetadata.totalSizeField(), new BEInteger(null, BigInteger.valueOf(message.getTotalSize().get())));
+            }
+        }});
+        m.writeTo(out);
+        if (message.getData().isPresent()) {
+            out.write(message.getData().get());
+        }
+    }
+
+    @Override
+    public int decode(DecodingContext context, ByteBuffer buffer) {
+        byte[] payload = new byte[buffer.remaining()];
+        buffer.get(payload);
+        try (BEParser parser = new BEParser(payload)) {
+            BEMap m = parser.readMap();
+            int length = m.getContent().length;
+            UtMetadata.Type messageType = getMessageType(m);
+            int pieceIndex = getPieceIndex(m);
+            switch (messageType) {
+                case REQUEST: {
+                    context.setMessage(UtMetadata.request(pieceIndex));
+                    return length;
+                }
+                case DATA: {
+                    byte[] data = Arrays.copyOfRange(payload, length, payload.length);
+                    context.setMessage(UtMetadata.data(pieceIndex, getTotalSize(m), data));
+                    return payload.length;
+                }
+                case REJECT: {
+                    context.setMessage(UtMetadata.reject(pieceIndex));
+                    return length;
+                }
+                default: {
+                    throw new IllegalStateException("Unknown message type: " + messageType.name());
+                }
+            }
+        }
+    }
+
+    private UtMetadata.Type getMessageType(BEMap m) {
+        BEInteger type = (BEInteger) m.getValue().get(UtMetadata.messageTypeField());
+        int typeId = type.getValue().intValueExact();
+        return UtMetadata.Type.forId(typeId);
+    }
+
+    private int getPieceIndex(BEMap m) {
+        return getIntAttribute(UtMetadata.pieceIndexField(), m);
+    }
+
+    private int getTotalSize(BEMap m) {
+        return getIntAttribute(UtMetadata.totalSizeField(), m);
+    }
+
+    private int getIntAttribute(String name, BEMap m) {
+        BEInteger value = ((BEInteger) m.getValue().get(name));
+        if (value == null) {
+            throw new IllegalStateException("Message attribute is missing: " + name);
+        }
+        return value.getValue().intValueExact();
+    }
+
+    @Override
+    public Collection<Class<? extends UtMetadata>> getSupportedTypes() {
+        return supportedTypes;
+    }
+
+    @Override
+    public Class<? extends UtMetadata> readMessageType(ByteBuffer buffer) {
+        return UtMetadata.class;
+    }
+}
