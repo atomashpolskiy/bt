f9219aa774d4f36d595f2496be34cac62f50b65e
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-06-28 21:17:32 +0300

    Generalize byte range API

--- bt-core/src/main/java/bt/data/range/Ranges.java
+++ bt-core/src/main/java/bt/data/range/Ranges.java
@@ -13,38 +13,15 @@ public class Ranges {
     /**
      * @since 1.3
      */
-    public static <T extends Range<T>> BlockRange<T> blockRange(T range, long blockSize) {
+    public static <T extends Range<T>> BlockRange<T> blockRange(Range<T> range, long blockSize) {
         return new BlockRange<>(range, blockSize);
     }
 
     /**
      * @since 1.3
      */
-    public static <T extends Range<T>> Range<T> synchronizedRange(T range) {
+    public static <T extends Range<T>> Range<T> synchronizedRange(Range<T> range) {
         return new SynchronizedRange<>(range);
-    }
-
-    /**
-     * @since 1.3
-     */
-    public static <T extends Range<T>> Range<BlockRange<T>> synchronizedBlockRange(T range, long blockSize) {
-        return new SynchronizedRange<>(blockRange(range, blockSize));
-    }
-
-    /**
-     * @since 1.3
-     */
-    public static <T extends Range<T>> DataRange synchronizedDataRange(T range, Function<T, DataRange> converter) {
-        SynchronizedRange<T> synchronizedRange = new SynchronizedRange<>(range);
-        return new SynchronizedDataRange<>(synchronizedRange, converter::apply);
-    }
-
-    /**
-     * @since 1.3
-     */
-    public static DataRange synchronizedDataRange(BlockRange<DataRange> range) {
-        SynchronizedRange<BlockRange<DataRange>> synchronizedRange = new SynchronizedRange<>(range);
-        return new SynchronizedDataRange<>(synchronizedRange, BlockRange::getDelegate);
     }
 
     /**
@@ -58,14 +35,22 @@ public class Ranges {
     /**
      * @since 1.3
      */
-    public static BlockSet synchronizedBlockSet(BlockSet blockSet) {
-        return new SynchronizedBlockSet(blockSet);
+    public static DataRange dataRange(BlockRange<DataRange> range) {
+        return DelegatingDataRange.range(range, DelegatingRange::getDelegate);
     }
 
     /**
      * @since 1.3
      */
-    public static DataRange dataRange(BlockRange<DataRange> range) {
-        return DelegatingDataRange.range(range, DelegatingRange::getDelegate);
+    public static DataRange synchronizedDataRange(BlockRange<DataRange> range) {
+        SynchronizedRange<BlockRange<DataRange>> synchronizedRange = new SynchronizedRange<>(range);
+        return new SynchronizedDataRange<>(synchronizedRange, BlockRange::getDelegate);
+    }
+
+    /**
+     * @since 1.3
+     */
+    public static BlockSet synchronizedBlockSet(BlockSet blockSet) {
+        return new SynchronizedBlockSet(blockSet);
     }
 }
