653fc7e5ef2f0ea1b15692bcac92f4f9371bfc31
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-07-11 20:59:36 +0300

    Maven skipTests option has no effect

--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -3,4 +3,4 @@
 eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
 export GPG_TTY=$(tty)
 export GPG_AGENT_INFO
-mvn -Darguments="-Dskip.unit.tests -Dskip.integration.tests -DskipTests" -Prelease release:prepare
+mvn -Darguments="-DskipTests" -Prelease release:prepare
