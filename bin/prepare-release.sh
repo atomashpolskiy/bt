123aa34cd38286bb834efe185ba39c5fa621e462
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-07-25 20:58:43 +0300

    autoVersionSubmodules in release

--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -3,4 +3,4 @@
 eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
 export GPG_TTY=$(tty)
 export GPG_AGENT_INFO
-mvn -Darguments="-DskipTests" -Prelease release:prepare
+mvn -Darguments="-DskipTests -DautoVersionSubmodules" -Prelease release:prepare
