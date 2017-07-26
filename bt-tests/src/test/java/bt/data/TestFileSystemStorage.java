62715f926b20476370f6655ed8da9c3a97c915b5
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-07-26 21:54:35 +0300

    windows build

--- bt-tests/src/test/java/bt/data/TestFileSystemStorage.java
+++ bt-tests/src/test/java/bt/data/TestFileSystemStorage.java
@@ -8,6 +8,8 @@ import org.junit.rules.ExternalResource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
 
 // TODO: replace this with in-memory storage
 class TestFileSystemStorage extends ExternalResource implements Storage {
@@ -18,17 +20,22 @@ class TestFileSystemStorage extends ExternalResource implements Storage {
     private Storage delegate;
     private final Object lock;
 
+    private Collection<StorageUnit> units;
+
     public TestFileSystemStorage() {
         rootDirectory = new File(ROOT_PATH);
         if (!rootDirectory.mkdirs()) {
             throw new RuntimeException("Failed to create directories: " + rootDirectory);
         }
+        units = new ArrayList<>();
         lock = new Object();
     }
 
     @Override
     public StorageUnit getUnit(Torrent torrent, TorrentFile torrentFile) {
-        return getDelegate().getUnit(torrent, torrentFile);
+        StorageUnit unit = getDelegate().getUnit(torrent, torrentFile);
+        units.add(unit);
+        return unit;
     }
 
     private Storage getDelegate() {
@@ -49,6 +56,9 @@ class TestFileSystemStorage extends ExternalResource implements Storage {
     @Override
     protected void after() {
         try {
+            for (StorageUnit unit : units) {
+                unit.close();
+            }
             deleteRecursive(rootDirectory);
         } catch (IOException e) {
             throw new RuntimeException(e);
