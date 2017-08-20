bed93fc9dc232d9eecf8178207171381c381bdf0
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-08-20 15:51:15 +0300

    remove duplicate script

--- script/deploy-ossrh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# previously it used to work automatically per mvn release:perform...
-# need to investigate why it doesn't upload to Sonatype anymore
-cd target/checkout
-mvn nexus-staging:deploy-staged
