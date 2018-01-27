94d31beeba9d35668f1ecb7bf32994795673151d
Andrei Tomashpolskiy <nordmann89@gmail.com>
2018-01-27 18:33:34 +0300

    Update scripts [skip ci]

--- script/prepare-release.sh
+++ script/prepare-release.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
+# In case there are several PGP keys, set GPG_KEYNAME to a particular keyid.
+# E.g. GPG_KEYNAME=FF001234 ./prepare-release.sh
+
+if [[ ${GPG_KEYNAME} == "" ]]
+then
+    echo "Using default PGP key..."
+else
+    GPG_KEYNAME="-Dgpg.keyname=${GPG_KEYNAME}"
+fi
+
+eval $(gpg-agent --daemon --no-grab)
 export GPG_TTY=$(tty)
 export GPG_AGENT_INFO
-mvn -Darguments="-DskipTests -DautoVersionSubmodules" -Prelease release:prepare
+mvn -Darguments="-DskipTests -DautoVersionSubmodules ${GPG_KEYNAME}" -Prelease release:prepare
