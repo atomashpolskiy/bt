b8a4b8a0ecb3f6abde2d13dc35563da2eb1c2213
Andrei Tomashpolskiy <nordmann89@gmail.com>
2019-06-14 15:44:53 +0300

    Disable LSD test on Travis

--- script/travis-run-tests.sh
+++ script/travis-run-tests.sh
@@ -4,11 +4,11 @@ export MAVEN_OPTS="-Xmx512m"
 
 if [ "$MAIN_BUILD" == "true" ]; then
 
-    mvn -U clean install -Pcodecov 2>&1 | grep -Ev 'DEBUG|TRACE'
+    mvn -U clean install -Pcodecov,travis 2>&1 | grep -Ev 'DEBUG|TRACE'
     exit ${PIPESTATUS[0]} # make sure that maven's exit code is returned to travis build
 
 elif [ "$TRAVIS_JDK_VERSION" == "oraclejdk9" ]; then
 
-    mvn -U clean install -Pjdk9
+    mvn -U clean install -Pjdk9,travis
 
 fi
