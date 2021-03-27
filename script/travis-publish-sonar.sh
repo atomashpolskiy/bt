40b9222e014485a68672b9c4af9b165366a240fd
Andrei Tomashpolskiy <nordmann89@gmail.com>
2021-03-28 01:08:50 +0300

    Update travis-publish-sonar.sh

--- script/travis-publish-sonar.sh
+++ script/travis-publish-sonar.sh
@@ -3,7 +3,8 @@
 if [ "$TRAVIS_REPO_SLUG" == "atomashpolskiy/bt" ] && [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$MAIN_BUILD" == "true" ]; then
 
     #  prevent showing the token in travis logs
-    mvn sonar:sonar -Dsonar.login=${SONAR_TOKEN} -Psonar > /dev/null 2>&1
+    # TODO: Sonar stopped accepting Java 8; need to upgrade to at least Java 11
+    #mvn sonar:sonar -Dsonar.login=${SONAR_TOKEN} -Psonar > /dev/null 2>&1
 
 else
 
